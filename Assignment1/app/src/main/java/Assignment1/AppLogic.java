22219ca86fafb2365ecf110c98a7fcaf732817a3
Marian Derias <marianderias@gmail.com>
2022-09-14 16:18:11 +1000

    fixed build

--- Assignment1/app/src/main/java/Assignment1/AppLogic.java
+++ Assignment1/app/src/main/java/Assignment1/AppLogic.java
@@ -10,9 +10,9 @@ public class AppLogic{
     private static Scanner scan = new Scanner(System.in);
     // A list of all the currencies in the program
     // Each currency is saved as a LinkedHashMap to maintain order of exchange rates
-    private static List<LinkedHashMap<String, Double>> currencies = new ArrayList<LinkedHashMap<String, Double>>();
+    static List<LinkedHashMap<String, Double>> currencies = new ArrayList<LinkedHashMap<String, Double>>();
     // A list of the popular currencies. Can be updated by admin
-    private static List<String> popCurrencies = new ArrayList<String>(){
+    static List<String> popCurrencies = new ArrayList<String>(){
         {
             add("USD");
             add("AUD");
