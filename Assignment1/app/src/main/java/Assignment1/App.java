c79dfe491bb74ad3b8c2c6b60fd692016705bb32
pbha6036 <pbha6036@uni.sydney.edu.au>
2022-09-19 01:16:52 +1000

    Added admin addNewCurrency and addExchangeRate methods. Made the whole program recursively continuous by using selectUserOption method.

--- Assignment1/app/src/main/java/Assignment1/App.java
+++ Assignment1/app/src/main/java/Assignment1/App.java
@@ -10,7 +10,6 @@ public class App {
         // Starting of the application
         String user = UserInterface.getString(Arrays.asList("1", "2"), "Welcome to Currency Converter! Select displayed options to continue!" +
                 "\nWhat is your user type?\n1: User\n2: Admin", new Scanner(System.in));
-        AppLogic.selectUserOption(user, new Scanner(System.in));
 
         //Make a password for admin
         if (user.equals("2")) {
