0937c8cd05d2c98b40d1d7b5f41c664794fb17ce
Marian Derias <marianderias@gmail.com>
2022-09-19 20:38:01 +1000

    moved MathsTests into LogicTest

--- Assignment1/app/src/test/java/LogicTest.java
+++ Assignment1/app/src/test/java/LogicTest.java
@@ -97,6 +97,44 @@ class LogicTest{
                 "\n2: Display currency table\n3: Summary of 2 currencies", scan));
 
     }
+
+    private ArrayList<Double> testNums = new ArrayList<Double>(Arrays.asList(4.35, 3.1415, 1.16457, 42.89, 5.0, 6.73));
+
+    // Each method has been tested to 6 d.p.
+    @Test
+    void testAverage(){
+        assertNotNull(new Operations());
+        double num = (double)Math.round(Operations.getAverage(testNums) * 1000000d) /1000000d;
+        assertEquals(String.valueOf(num), "10.546012");
+    }
+    @Test
+    void testEvenMedian(){
+        double num = (double)Math.round(Operations.getMedian(testNums) * 1000000d) /1000000d;
+        assertEquals(String.valueOf(num), "4.675");
+    }
+    @Test
+    void testOddMedian(){
+        testNums.add(5.6);
+        double num = (double)Math.round(Operations.getMedian(testNums) * 1000000d) /1000000d;
+        assertEquals(String.valueOf(num), "5.0");
+        testNums.remove(5.6);
+    }
+    @Test
+    void testMax(){
+        double num = (double)Math.round(Operations.getMaximum(testNums) * 1000000d) /1000000d;
+        assertEquals(String.valueOf(num), "42.89");
+    }
+    @Test
+    void testMin(){
+        double num = (double)Math.round(Operations.getMinimum(testNums) * 1000000d) /1000000d;
+        assertEquals(String.valueOf(num), "1.16457");
+    }
+
+    @Test
+    void testStandardDeviation(){
+        double num = (double)Math.round(Operations.getSD(testNums) * 1000000d) /1000000d;
+        assertEquals(String.valueOf(num), "14.564126");
+    }
 }
 
 
