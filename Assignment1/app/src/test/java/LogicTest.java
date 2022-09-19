039072339bc68cf7ce6f87ecae96ffb8d6b25b29
pbha6036 <pbha6036@uni.sydney.edu.au>
2022-09-19 17:08:43 +1000

    Troubleshooting tests

--- Assignment1/app/src/test/java/LogicTest.java
+++ Assignment1/app/src/test/java/LogicTest.java
@@ -8,38 +8,6 @@ import java.lang.Exception;
 
 class LogicTest{
 
-//    private final PrintStream standardOut = System.out;
-//    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
-//
-//    // Redirects stdout stream to a new print stream to help check terminal output
-//    @BeforeEach
-//    public void setUp() {
-//        System.setOut(new PrintStream(outputStreamCaptor));
-//    }
-//
-//    // Resets the output stream after each test to prevent involvement with other code
-//    @AfterEach
-//    public void tearDown() {
-//        System.setOut(standardOut);
-//    }
-
-//    @Test
-//    public void currencyListCheck() throws Exception{
-//        assertNotNull(new AppLogic());
-//        AppLogic.readCurrencyFiles();
-//        assertEquals(AppLogic.getCurrenciesHashMaps().size(), 6);
-//        ArrayList<String> allCurrencies = new ArrayList<String>();
-//        allCurrencies.add("AUD");
-//        allCurrencies.add("SGD");
-//        allCurrencies.add("EUR");
-//        allCurrencies.add("USD");
-//        allCurrencies.add("JPY");
-//        allCurrencies.add("ILS");
-//
-//        assertEquals(AppLogic.getAllCurrencies(), allCurrencies);
-//    }
-
-
 // Tests the number of hashmaps produced by method against manual calculation of the number of filepaths in currencyFiles.txt
     @Test
     public void currencyListCheck() throws Exception {
@@ -87,66 +55,6 @@ class LogicTest{
         AppLogic.summaryOf2Currencies(new Scanner(input));
     }
 
-    // Tests "addNewCurrency" method in AppLogic
-//    @Test
-//    public void addNewCurrency() throws Exception {
-//        // outputStreamCaptor.toString().trim()
-//        AppLogic.addNewCurrency(new Scanner("AUD\n" + "6\n"));
-//        assertEquals("Please input name of the currency you want to add.\nError: that currency already exists in the system.\n", outputStreamCaptor.toString());
-
-//        // Deleting the test file created by method
-//        File testFile = new File("./src/main/java/Assignment1/currencies/TEST.csv");
-//        assertTrue(testFile.delete());
-//
-//        // "Deleting" the added line from currencyList.txt
-//        File inputFile = new File("./src/main/java/Assignment1/currencies/currencyFiles.txt");
-//        File tempFile = new File("myTempFile.txt");
-//
-//        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-//        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
-//
-//        String lineToRemove = "./src/main/java/Assignment1/currencies/TEST.csv\n";
-//        String currentLine;
-//
-//        while((currentLine = reader.readLine()) != null) {
-//            // trim newline when comparing with lineToRemove
-//            if(currentLine.equals(lineToRemove)) continue;
-//            writer.write(currentLine);
-//        }
-//        writer.close();
-//        reader.close();
-//        inputFile.delete();
-//        boolean successful = tempFile.renameTo(new File("./src/main/java/Assignment1/currencies/currencyFiles.txt"));
-//
-//        assertTrue(successful);
-//    }
-
-//    @Test
-//    public void addExchangeRate() throws Exception{
-//        AppLogic.addExchangeRate(new Scanner("test\n" + "6\n"));
-//        assertEquals("Please input currency name.\nCurrency does not exist.", outputStreamCaptor.toString());
-
-//        // "Deleting" the added line from currencyList.txt
-//        File inputFile = new File("./src/main/java/Assignment1/currencies/AUD.csv");
-//        File tempFile = new File("myTempFile.txt");
-//
-//        BufferedReader reader = new BufferedReader(new FileReader(inputFile));
-//        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile));
-//
-//        String lineToRemove = "testDate, 1.12345\n";
-//        String currentLine;
-//
-//        while((currentLine = reader.readLine()) != null) {
-//            // trim newline when comparing with lineToRemove
-//            if(currentLine.equals(lineToRemove)) continue;
-//            writer.write(currentLine);
-//        }
-//        writer.close();
-//        reader.close();
-//        inputFile.delete();
-//        boolean successful = tempFile.renameTo(new File("./src/main/java/Assignment1/currencies/AUD.csv"));
-//        assertTrue(successful);
-//    }
 
     @Test
     public void selectUserOption() {
